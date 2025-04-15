797d00ab4fd0045db18f2b38933cbf082f8b702d
Phil Hawksworth <phil@deno.com>
2025-04-15 16:17:11 +0100

    add explicit response code

--- main.ts
+++ main.ts
@@ -3,12 +3,14 @@ const app = new Hono()
 
 app.get('/', (c) => {
   console.log("GET /");
+  c.status(200);
   return c.text('Hello, support bot!')
 })
 
 app.post('/new', async (c) => {
   const data = await c.req.json()
   console.log("POST /new", data);
+  c.status(200);
   return c.text('POST /new')
 })
 
